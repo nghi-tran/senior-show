1b9d9172f4241d9a70fdfad469424feb3e0492b5
unknown <cwodynsk@asu.edu>
2022-03-02 18:30:29 -0700

    Made the desktop footer

--- front-page.php
+++ front-page.php
@@ -7,5 +7,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
--- header.php
+++ header.php
@@ -25,35 +25,15 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'seniorshow' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$seniorshow_description = get_bloginfo( 'description', 'display' );
-			if ( $seniorshow_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $seniorshow_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<header>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'seniorshow' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+        <div class="navbar">
+            <div class="navbar__menu">
+                <a href="#">About</a>
+                <a href="#">Donors</a>
+                <a href="#">Social Issue</a>
+            </div>
+        </div>
+        
+        
 	</header><!-- #masthead -->
