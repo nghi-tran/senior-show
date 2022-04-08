67ef4b16da0e330513f659d26765a825822192c0
Nghi Tran <ndtran3@asu.edu>
2022-04-08 14:51:39 -0700

    added padding

--- template-about.php
+++ template-about.php
@@ -14,6 +14,9 @@ get_header();
 					<div class="about__class-photo">
 							<img src="<?php echo get_template_directory_uri(); ?>/img/class-photo.png" alt="POV Horizontal Logo" />
 					</div>
+					<div class="about__class-photo--mobile">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/class-photo-mobile.png" alt="POV Horizontal Logo" />
+					</div>
 
 					<div class="show-about">
 						<div class="show-about__title">
@@ -24,7 +27,7 @@ get_header();
 
 						<div class="show-about__description">
 							<div class="show-about__description-paragraph">
-								<h2>
+								<p>
 									The Graphic Design Senior Student Association (GDSSA) is the senior
 									class in the Visual Communication Design Program at Arizona State
 									University. We are a resilient group of 44 passionate designers who
@@ -33,12 +36,13 @@ get_header();
 									the 30th Annual Senior Exhibition, the culmination of a year’s worth of
 									research, thinking, and design around social issues we are passionate
 									about to raise awareness and spark conversations.
-								</h2>
+								</p>
 							</div>
 
 							<div class="about__class-photo">
 									<img src="<?php echo get_template_directory_uri(); ?>/img/show-about.jpg" alt="POV Horizontal Logo" />
 							</div>
+
 						</div>
 
 					</div><!-- closes show-about -->
@@ -48,10 +52,10 @@ get_header();
 							<div class="show-location__title"><h1>Location</h1></div>
 							<div class="show-location__venue"><h2>Park Central Mall</h2></div>
 							<div class="show-location__address">
-								<h2>
+								<p>
 								3121 N 3rd Ave <br>
 								Phoenix, AZ, 85013
-								</h2>
+							</p>
 							</div>
 						</div>
 
@@ -69,25 +73,25 @@ get_header();
 						<div class="show-date__description-wrapper">
 							<div class="show-date__title"><h1>Date</h1></div>
 							<div class="show-date__duration">
-								<h2>Wednesday, May 4th <br>
+								<p>Wednesday, May 4th <br>
 									to Sunday, May 8th
-								</h2>
+								</p>
 							</div>
 							<div class="show-date__time">
-								<h2>
+								<p>
 								10:00AM - 5:00PM
-								</h2>
+							</p>
 							</div>
 						</div>
 
 						<div class="show-date__content-wrapper">
 							<div class="show-date__img-1">
-									<h2>
+									<p>
 										We are delighted to invite you to share this experience with us
 										as we explore the intricacies of our society and celebrate the
 										work of our graduating seniors.
 
-									</h2>
+									</p>
 							</div>
 						</div>
 					</div> <!-- closes show__location -->
